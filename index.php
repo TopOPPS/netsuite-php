6e398731711de3abbddfecbfd0a31a8f6705721f
emeth- <seanybob@gmail.com>
2016-06-20 13:02:03 -0500

    how about 'dis here index file?

---
+++ index.php
@@ -0,0 +1,40 @@
+<?
+require 'vendor/autoload.php';
+
+use NetSuite\NetSuiteService;
+
+$config = array(
+   // required -------------------------------------
+   "endpoint" => "2016_1",
+   "host"     => "https://webservices.netsuite.com",
+   "email"    => $_GET['email'],
+   "password" => $_GET['password'],
+   "role"     => "3",
+   "account"  => $_GET['account'],
+   "app_id"   => "", //4AD027CA-88B3-46EC-9D3E-41C6E6A325E2
+   // optional -------------------------------------
+   "logging"  => true,
+   "log_path" => ""
+);
+
+$service = new NetSuiteService($config);
+
+use NetSuite\Classes\GetRequest;
+use NetSuite\Classes\RecordRef;
+
+$request = new GetRequest();
+$request->baseRef = new RecordRef();
+$request->baseRef->internalId = $_GET['opp_id'];
+$request->baseRef->type = "opportunity";
+
+$getResponse = $service->get($request);
+
+if ( ! $getResponse->readResponse->status->isSuccess) {
+    echo "GET ERROR";
+} else {
+    $customer = $getResponse->readResponse->record;
+}
+
+print_r($customer);
+
+?>
