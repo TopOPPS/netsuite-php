a7627a14950956b20f5c35d1872fce0acb23e075
emeth- <seanybob@gmail.com>
2016-06-20 13:06:14 -0500

    application id appears to be necessary

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $config = array(
    "password" => $_GET['password'],
    "role"     => "3",
    "account"  => $_GET['account'],
-   "app_id"   => "", //4AD027CA-88B3-46EC-9D3E-41C6E6A325E2
+   "app_id"   => "4AD027CA-88B3-46EC-9D3E-41C6E6A325E2",
    // optional -------------------------------------
    "logging"  => true,
    "log_path" => ""
