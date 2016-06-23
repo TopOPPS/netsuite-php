26250d054f1b87d9c6ef4b2e31248045295ca88a
emeth- <seanybob@gmail.com>
2016-06-23 10:17:47 -0500

    added auth

--- authenticate.php
+++ authenticate.php
@@ -9,7 +9,7 @@ $config = array(
    "host"     => "https://webservices.netsuite.com",
    "email"    => $_GET['email'],
    "password" => $_GET['password'],
-   "role"     => "3",
+   "role"     => $_GET['role'],
    "account"  => $_GET['account'],
    "app_id"   => "4AD027CA-88B3-46EC-9D3E-41C6E6A325E2",
    // optional -------------------------------------
@@ -17,28 +17,44 @@ $config = array(
    "log_path" => ""
 );
 
-print "1";
+/*
+http://localhost:8888/authenticate.php?email=sean+peon@topopps.com&password=Topd0ge1!!!&account=TSTDRV1496099&title_starts_with=&role=9
+ */
+
 $service = new NetSuiteService($config);
+
+use NetSuite\Classes\SearchStringField;
+use NetSuite\Classes\OpportunitySearchBasic;
+use NetSuite\Classes\SearchRequest;
+
+$service->setSearchPreferences(false, 20);
+
+$oppSearchField = new SearchStringField();
+$oppSearchField->operator = "startsWith";
+$oppSearchField->searchValue = $_GET['title_starts_with'];
+
+$search = new OpportunitySearchBasic();
+$search->title = $oppSearchField;
+
+$request = new SearchRequest();
+$request->searchRecord = $search;
+
+print "1";
+try {    
+    $searchResponse = $service->search($request);
+} catch (Exception $e) {
+    
+}
 print "2";
 
-use NetSuite\Classes\GetRequest;
-use NetSuite\Classes\RecordRef;
-
-$request = new GetRequest();
-$request->baseRef = new RecordRef();
-$request->baseRef->internalId = $_GET['opp_id'];
-$request->baseRef->type = "opportunity";
-
-$getResponse = $service->get($request);
-
-if ( ! $getResponse->readResponse->status->isSuccess) {
-    print "4";
-    echo "GET ERROR";
+if (!$searchResponse->searchResult->status->isSuccess) {
+    echo "SEARCH ERROR";
 } else {
-    print "5";
-    $customer = $getResponse->readResponse->record;
-}
+    $result = $searchResponse->searchResult;
+    $count = $result->totalRecords;
+    $records = $result->recordList;
 
-print json_encode($customer);
+    print json_encode($result);
+}
 
 ?>
